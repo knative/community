eabcc28763915edb60aafdb1640775df526f1c5f
Nghia Tran <tcnghia@gmail.com>
2020-10-21 17:57:50 -0700

    Extract loading to func

--- mechanics/tools/gdocs-migrate/main.go
+++ mechanics/tools/gdocs-migrate/main.go
@@ -8,11 +8,56 @@ import (
 	"google.golang.org/api/drive/v3"
 )
 
-const (
-	KnativeCommunityDriveID = "0APnJ_hRs30R2Uk9PVA"
-	KnativeTeamDriveID      = "0AM-QGZJ-HUA8Uk9PVA"
-	FolderMimeType          = "application/vnd.google-apps.folder"
+var (
+	knativeCommunity = File{
+		id:   "0APnJ_hRs30R2Uk9PVA",
+		name: "Knative Community Drive",
+	}
+	knativeTeam = File{
+		id:   "0AM-QGZJ-HUA8Uk9PVA",
+		name: "Knative Team Drive",
+	}
 )
+
+const (
+	FolderMimeType = "application/vnd.google-apps.folder"
+)
+
+func loadAllFiles(forest *Forest, srv *drive.Service, drive File) {
+	forest.Add(File{
+		id:       drive.id,
+		name:     drive.name,
+		isFolder: true,
+	})
+	var pageToken *string
+	for pageToken == nil || *pageToken != "" {
+		// List files from Knative Community Drive.
+		request := srv.Files.List().PageSize(1000).
+			Corpora("drive").
+			IncludeItemsFromAllDrives(true).
+			SupportsAllDrives(true).
+			DriveId(drive.id).
+			Fields("nextPageToken, files(id, name, parents, mimeType)").
+			OrderBy("folder,modifiedTime desc,name")
+		if pageToken != nil {
+			request.PageToken(*pageToken)
+		}
+		r, err := request.Do()
+		if err != nil {
+			log.Fatal("Unable to retrieve files:", err)
+		}
+		pageToken = &r.NextPageToken
+		for _, i := range r.Files {
+			forest.Add(File{
+				id:       i.Id,
+				name:     i.Name,
+				parentID: i.Parents[0],
+				isFolder: i.MimeType == FolderMimeType,
+			})
+		}
+		fmt.Printf("Loaded %d files, %d folders\n", len(forest.GetFiles()), forest.FolderCount())
+	}
+}
 
 func main() {
 	// You need to put credentials into a file named credentials.json.
@@ -27,47 +72,10 @@ func main() {
 		log.Fatal("Unable to retrieve Drive client:", err)
 	}
 
-	drives := map[string]string{
-		"Knative Community Drive": KnativeCommunityDriveID,
-		"Knative Team Drive":      KnativeTeamDriveID,
-	}
-
 	forest := NewForest()
-	for name, id := range drives {
-		fmt.Println("Loading directory from ... ", name)
-		forest.Add(File{
-			id:       id,
-			name:     name,
-			isFolder: true,
-		})
-		var pageToken *string
-		for pageToken == nil || *pageToken != "" {
-			// List files from Knative Community Drive.
-			request := srv.Files.List().PageSize(1000).
-				Corpora("drive").
-				IncludeItemsFromAllDrives(true).
-				SupportsAllDrives(true).
-				DriveId(id).
-				Fields("nextPageToken, files(id, name, parents, mimeType)").
-				OrderBy("folder,modifiedTime desc,name")
-			if pageToken != nil {
-				request.PageToken(*pageToken)
-			}
-			r, err := request.Do()
-			if err != nil {
-				log.Fatal("Unable to retrieve files:", err)
-			}
-			pageToken = &r.NextPageToken
-			for _, i := range r.Files {
-				forest.Add(File{
-					id:       i.Id,
-					name:     i.Name,
-					parentID: i.Parents[0],
-					isFolder: i.MimeType == FolderMimeType,
-				})
-			}
-			fmt.Printf("Loaded %d files, %d folders\n", len(forest.GetFiles()), forest.FolderCount())
-		}
+	for _, drive := range []File{knativeTeam, knativeCommunity} {
+		fmt.Println("Loading directory from ... ", drive.name)
+		loadAllFiles(&forest, srv, drive)
 	}
 
 	for _, f := range forest.GetFiles() {
